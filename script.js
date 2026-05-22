53d00f121afd86871f9b9e16456d44d07b6fe94c
Brayan valderrama <rubianostivenbrayan13@gmail.com>
2026-05-21 20:52:54 -0500

    cambio de hotfix

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
-});
- 
+}); 
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
